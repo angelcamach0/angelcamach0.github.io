953c98674829d51a0e5e0c6b66c858396ef19f36
Angel <angelcam@nmsu.edu>
2020-04-29 19:18:21 -0600

    just added a gif and a button

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,4 +1,7 @@
 
         for( n = 0; n< 100 ; n++ ){
             document.body.innerHTML = "This is some content";
+            document.getElementById("demo").innerHTML = 5 + 6;
         }
+
+        
